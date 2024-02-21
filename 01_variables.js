37db7e5efd41830866a3b38b87293377abc72caa
01gourav <160702351+01gourav@users.noreply.github.com>
2024-02-21 12:32:55 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 4455698
+let accountEmail = "gbkhajja@gmail.com"
+var accountPassword = "12354"
+accountCity = "Banswara"
+let accountState;
+
+
+// accountId = 2
+accountEmail = "hh@gmail.com"
+accountPassword = "554551"
+accountCity = "hyderbad"
+console.log([accountEmail, accountId, accountPassword, accountCity, accountState])
---
+++ 02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//alert (3+3)
+console.log(3 +3);
+console.log("gourav");
+let name = "gourav"
+let age = 22
+let isLoggedIn = "false"
+
+console.log(typeof false);
