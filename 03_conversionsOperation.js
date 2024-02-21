f4ad35e64a8bd014016d830223e9f5c761fb1d61
01gourav <160702351+01gourav@users.noreply.github.com>
2024-02-21 13:09:30 +0530

    Add files via upload

---
+++ 03_conversionsOperation.js
@@ -0,0 +1,17 @@
+let score = "gourav"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+let isLoggedIn = 1 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
